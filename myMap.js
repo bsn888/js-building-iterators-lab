e3c8b69b9cf40988b26719eea398b1c21434fc67
bsn888 <brian.nace@hushmail.com>
2016-11-08 08:36:59 -0700

    i do not get it

--- myMap.js
+++ myMap.js
@@ -1,9 +1,12 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Map
 
 function myMap(arr, callback) {
+	var outArray = [];
+	for (var i = 0; i < arr.length; i++){
+		outArray[i] = callback (arr[i], i, arr);
 
-//  CODE INSIDE HERE   //
-
+}
+return outArray;
 }
 
 /*
--- myReduce.js
+++ myReduce.js
@@ -1,12 +1,9 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
-
-//  CODE INSIDE HERE   //
-
-}
-
+function myReduce(arr, callback, initialValue) {
+	var i=0;  // setting this here so we can increment it +1 when no initialValue
+  
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
@@ -19,4 +16,5 @@ function myReduce(arr, callback) {
 
 
 // export this function (you can ignore this for now)
-module.exports = myReduce;
+// module.exports = myReduce;//
+// 
